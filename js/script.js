8f1f0af60fb8e011351df3a8390f72965d12580a
Rikta Islam <rikta9150@gmail.com>
2024-03-21 20:20:57 +0600

    API add

--- js/script.js
+++ js/script.js
@@ -1,30 +1,61 @@
-const form = document.querySelector("form");
-const Name = document.querySelector("#Name");
+// ---------- portfolio project start ------------
+function loadProject(){
+    fetch('../Data/project.json')
+    .then(res => res.json())
+    .then(data => displayProject(data))
+}
+loadProject();
 
-const email = document.querySelector("#Email");
-const Subject = document.querySelector("#Subject");
-const Message = document.querySelector("#Message");
+function displayProject(data) {
+    const allProject = document.getElementById('project')
 
-form.addEventListener("submit", formHandler);
+    for(const protfolio of data){
+        const div = document.createElement('div');
+        div.innerHTML = `
+      
+        <div class="shadow pb-4 mb-4">
+        <div class="item" style="height: 320px; overflow: hidden;">
+        <img src="${protfolio.images}" class="img-fluid mb-4 w-full  rounded" alt="" >
+        <span><a href="${protfolio.images}" target="_blank"><i class="fa-solid fa-plus"></i></a></span>
+      </div>
+      <a href="${protfolio.live_site}" target="_blank" class="btn btn-primary text-white mt-3 py-2">< GO Live site /></a>
+       </div>
+    
+    
+    
+
+  
+        `;
+        allProject.appendChild(div);
+    }
+}
+// ---------- portfolio project End ------------
+//===============================================
+// ---------- contact from start -----------------
+const form = document.querySelector('form');
+const firstName = document.querySelector('#fistName');
+const emailAddress = document.querySelector('#exampleInputEmail1');
+const subject = document.querySelector('#subject');
+const message = document.querySelector('#message');
+
+form.addEventListener('submit', formHandler);
 
 
 function formHandler(e){
     e.preventDefault();
-    
 
     const formInfo = {
-        Name: Name.value,
-       
-        Email: email.value,
-        Subject: Subject.value,
-        Message: Message.value
+        FistName: firstName.value,
+        Email: emailAddress.value,
+        Subject: subject.value,
+        Message: message.value
     }
 
     console.log(formInfo);
 
-    Name.vlue = "",
-    
-    email = "",
-    Subject.value = "",
-    Message.value = ""
+    firstName.value = '',
+    emailAddress.value = '',
+    subject.value = '',
+    message.value = ''
 }
+// ---------- contact from end ------------
